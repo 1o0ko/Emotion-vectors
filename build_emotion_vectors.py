a8e9b7cd2900ddeb0706704af2f96aa3fb76b6bb
1o0ko <wojciech.stokowiec@gmail.com>
2016-02-28 02:47:00 +0100

    fixed cp paste bug

--- build_emotion_vectors.py
+++ build_emotion_vectors.py
@@ -10,10 +10,13 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 def getSentenceVector(sentence, params):    
     word_list = word_tokenize(sentence)
-            
+    
+    model = params['model']
+    blacklist = params['blacklist']
+    
     filtered_words = [word.lower() for word in word_list if word.lower() not in blacklist]                       
     sentence_vector = sum([model[word] for word in filtered_words if word in model])
-                                    
+    
     return sentence_vector
 
 def getVector(tree, params):
@@ -22,20 +25,22 @@ def getVector(tree, params):
     return model[word] if word in model else np.zeros(model.layer1_size)
 
 def tree2vector(tree, params, f = getVector):        
-                                                    
+    
     def getvector(tree, current_weight, level):                                     
-                                                                    
+        
         tree_value = f(tree, params)
-                                                                                
+        
         node_value =  tree_value
         node_weight = 1.0 / len(tree.children) if tree.children else 1               
+
         if tree.children:        
             for leaf in tree.children:      
                 node_value += node_weight*getvector(leaf, node_weight, level + 1)
         else:
             node_value = tree_value   
-            return node_value / level
-
+        
+        return node_value / level
+    
     return getvector(tree, 1, 1)
 
 def getEmotionVectors(path, params):
@@ -44,19 +49,20 @@ def getEmotionVectors(path, params):
         if file.endswith(".pickle"):
             tree = load('{0}/{1}'.format(path, file))
             emotions[file[:file.find('.')]] = tree2vector(tree, params)
-    
+
     return emotions
 
 def closest(sentence_vector, emotions):
-
+    
     closest = None
     best_distane = -float('inf')
+
     for name, emotion_vector in emotions.iteritems():        
         current_distance = cosine_similarity([sentence_vector], [emotion_vector])[0][0]    
         if best_distane < current_distance:
             best_distane = current_distance
             closest = name
-
+    
     return closest
 
 if __name__ == '__main__':
