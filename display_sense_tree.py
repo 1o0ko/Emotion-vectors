e61b99dd4653d5de42bc9a15b2f6e8a06b56aa09
1o0ko <wojciech.stokowiec@gmail.com>
2016-04-04 22:03:08 +0200

    It's working now

--- display_sense_tree.py
+++ display_sense_tree.py
@@ -3,12 +3,12 @@ import sys
 
 # to later save the sense tree
 def save(tree, fileName):
-        with open(fileName, 'wb') as handle:
+    with open(fileName, 'wb') as handle:
                 pickle.dump(tree, handle)
 # to later load the sense tree
 def load(fileName):
-        with open(fileName, 'rb') as handle:
-                return  pickle.load(handle)
+    with open(fileName, 'rb') as handle:
+        return  pickle.load(handle)
 
 
 def main(argv):
